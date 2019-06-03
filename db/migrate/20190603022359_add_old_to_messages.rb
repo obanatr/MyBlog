c052d011a990379d30e94f15284dfb0854ca7a6b
EC2 Default User <ec2-user@ip-172-31-47-68.us-east-2.compute.internal>
2019-06-03 03:17:22 +0000

    new-column

---
+++ db/migrate/20190603022359_add_old_to_messages.rb
@@ -0,0 +1,5 @@
+class AddOldToMessages < ActiveRecord::Migration[5.2]
+  def change
+    add_column :messages, :old, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_075159) do
+ActiveRecord::Schema.define(version: 2019_06_03_022359) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_06_01_075159) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "old"
   end
 
 end
